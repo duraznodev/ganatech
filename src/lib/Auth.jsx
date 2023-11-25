a7999e513f68e9aa0c090add02ba1ddaea832620
Roman Rizo <romanrizodev@gmail.com>
2023-11-25 09:39:28 -0600

    feat: add LoadingSkeleton component and update Auth.jsx

--- src/lib/Auth.jsx
+++ src/lib/Auth.jsx
@@ -1,12 +1,24 @@
 import Layout from "@/components/Layout";
 import { Navigate } from "react-router-dom";
 import { NewFarm } from "../routes";
+import { useState } from "react";
+import { useEffect } from "react";
+import LoadingSkeleton from "../components/LoadingSkeleton";
 
 function render(c) {
   return c;
 }
 
 export function Private(Component, user, farm_id) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user && farm_id) {
+      setLoading(false);
+    }
+  }, [user, farm_id]);
+
+  if (loading) return <LoadingSkeleton />;
   if (!user) return <Navigate to="/login" />;
   if (user && !farm_id) {
     return <NewFarm />;
